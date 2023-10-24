27f870548633322080a0e3230f796453158513fc
JPAKx4 <superpizzaman10@gmail.com>
2023-10-24 14:42:13 -0500

    Updated branch to have parity with main. Resolved all merge conflicts.

--- common/src/main/kotlin/com/cobblemon/mod/common/Cobblemon.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/Cobblemon.kt
@@ -132,7 +132,7 @@ import org.apache.logging.log4j.LogManager
 
 object Cobblemon {
     const val MODID = CobblemonConstants.MOD_ID
-
+    const val VERSION = "1.4.0"
     const val CONFIG_PATH = "config/$MODID/main.json"
     val LOGGER = LogManager.getLogger()
 
